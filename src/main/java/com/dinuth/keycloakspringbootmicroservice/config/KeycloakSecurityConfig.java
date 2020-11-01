4d631cfacf865316f3af58342d3cd17d8564cb6e
Jesus Javier Puente <chuchip@gmail.com>
2020-11-01 07:37:26 +0100

    Iniciando proyecto keycloack

---
+++ src/main/java/com/dinuth/keycloakspringbootmicroservice/config/KeycloakSecurityConfig.java
@@ -0,0 +1,68 @@
+	package com.dinuth.keycloakspringbootmicroservice.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+@KeycloakConfiguration
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+            .antMatchers("/test/anonymous").permitAll()
+            .antMatchers("/test/user").hasAnyRole("user")
+            .antMatchers("/test/admin").hasAnyRole("admin")
+            .antMatchers("/test/all-user").hasAnyRole("user","admin")
+            .anyRequest()
+            .permitAll();
+        http.csrf().disable();
+    }
+    
+    @Bean
+    @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public AccessToken accessToken() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return ((KeycloakSecurityContext) ((KeycloakAuthenticationToken) request.getUserPrincipal()).getCredentials()).getToken();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
---
+++ src/main/java/com/dinuth/keycloakspringbootmicroservice/controller/TestController.java
@@ -0,0 +1,66 @@
+package 	com.dinuth.keycloakspringbootmicroservice.controller;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.security.RolesAllowed;
+
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	AccessToken accessToken;
+	
+    @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
+    public ResponseEntity<String> getAnonymous() {
+    	log.info("Pasando por get de Anomimous");;
+        return ResponseEntity.ok("Hello Anonymous");
+    }
+
+    @RolesAllowed({"app-user","user"})
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<String> getUser(@RequestHeader String Authorization,Principal principal,Authentication auth) {
+//    	AccessToken accessToken= ((KeycloakSecurityContext) ((KeycloakAuthenticationToken) auth).getCredentials()).getToken();
+
+    	if (accessToken==null)
+    		log.info("Sin acceso");
+    	else
+    	{
+    		var  roles = accessToken.getResourceAccess();
+    		roles.forEach((k,v) -> log.info("Rol: "+v.getRoles().toString()));
+    		log.info("Nombre del principal Principal {} ",auth.getDetails().toString() );
+    	}
+    	
+
+        return ResponseEntity.ok("Hello User: "+Authorization);
+    }
+
+    @RolesAllowed("admin")
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ResponseEntity<String> getAdmin(@RequestHeader String Authorization) {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @RolesAllowed({ "admin", "user" })
+    @RequestMapping(value = "/all-user", method = RequestMethod.GET)
+    public ResponseEntity<String> getAllUser(@RequestHeader String Authorization) {
+        return ResponseEntity.ok("Hello All User");
+    }
+
+}
